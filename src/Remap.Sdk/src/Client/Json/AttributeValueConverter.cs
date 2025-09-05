2ae4cbdbefd5dbd37369a4dea10a122f2118038c
dkindev <omigoneo@gmail.com>
2025-09-06 00:45:43 +0300

    Refactor AttributeValueConverter

--- src/Remap.Sdk/src/Client/Json/AttributeValueConverter.cs
+++ src/Remap.Sdk/src/Client/Json/AttributeValueConverter.cs
@@ -5,84 +5,23 @@ using Newtonsoft.Json.Linq;
 
 namespace Confiti.MoySklad.Remap.Client.Json
 {
-    /// <summary>
-    /// Represents an json converter to <see cref="AttributeValue" />.
-    /// </summary>
-    public class AttributeValueConverter : JsonConverter<AttributeValue>
+    /// <inheritdoc/>
+    public class AttributeValueConverter : JsonCreationConverter<AttributeValue>
     {
-        #region Fields
-
-        private bool _canRead = true;
-
-        #endregion Fields
-
-        #region Properties
-
-        /// <summary>
-        /// Gets a value indicating whether this <see cref="JsonConverter"/> can read JSON.
-        /// </summary>
-        /// <value><c>true</c> if this <see cref="JsonConverter"/> can read JSON; otherwise, <c>false</c>.</value>
-        public override bool CanRead => _canRead;
-
-        /// <summary>
-        /// Gets a value indicating whether this <see cref="JsonConverter"/> can write JSON.
-        /// </summary>
-        /// <value><c>true</c> if this <see cref="JsonConverter"/> can write JSON; otherwise, <c>false</c>.</value>
-        public override bool CanWrite => false;
-
-        #endregion Properties
-
         #region Methods
 
-        /// <summary>
-        /// Reads the JSON representation of the object.
-        /// </summary>
-        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
-        /// <param name="objectType">Type of the object.</param>
-        /// <param name="existingValue">The existing value of object being read. If there is no existing value then <c>null</c> will be used.</param>
-        /// <param name="hasExistingValue">The existing value has a value.</param>
-        /// <param name="serializer">The calling serializer.</param>
-        /// <returns>The object value.</returns>
-        public override AttributeValue ReadJson(JsonReader reader, Type objectType, AttributeValue existingValue, bool hasExistingValue, JsonSerializer serializer)
+        /// <inheritdoc/>
+        protected override AttributeValue Create(Type objectType, JObject jObject, JsonSerializer serializer)
         {
-            switch (reader.TokenType)
-            {
-                case JsonToken.StartObject:
-                    return ReadAttributeValueObject(reader, serializer);
-
-                case JsonToken.Null:
-                    return null;
-            }
-
-            throw new JsonSerializationException($"Unexpected token when reading '{nameof(AttributeValue)}'.");
+            return new AttributeValue();
         }
 
-        /// <summary>
-        /// Writes the JSON representation of the object.
-        /// </summary>
-        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
-        /// <param name="value">The value.</param>
-        /// <param name="serializer">The calling serializer.</param>
-        public override void WriteJson(JsonWriter writer, AttributeValue value, JsonSerializer serializer)
+        /// <inheritdoc/>
+        protected override void PostPopulate(AttributeValue attributeValue, JsonSerializer serializer)
         {
-            throw new NotImplementedException();
-        }
-
-        #endregion Methods
-
-        #region Utilities
-
-        private AttributeValue ReadAttributeValueObject(JsonReader reader, JsonSerializer serializer)
-        {
-            _canRead = false;
-            var attributeValue = serializer.Deserialize<AttributeValue>(reader);
-            _canRead = true;
-
-            JToken jToken = null;
-            if (attributeValue.Value is JObject)
-                jToken = attributeValue.Value as JToken;
-            else
-                jToken = new JValue(attributeValue.Value);
+            var jToken = attributeValue.Value is JObject
+                ? attributeValue.Value as JToken
+                : new JValue(attributeValue.Value);
 
             switch (attributeValue.Type)
             {
@@ -150,10 +89,8 @@ namespace Confiti.MoySklad.Remap.Client.Json
                     attributeValue.Value = jToken.ToObject<string>(serializer);
                     break;
             }
-
-            return attributeValue;
         }
 
-        #endregion Utilities
+        #endregion Methods
     }
 }
--- src/Remap.Sdk/src/Client/Json/JsonCreationConverter.cs
+++ src/Remap.Sdk/src/Client/Json/JsonCreationConverter.cs
@@ -5,47 +5,33 @@ using Newtonsoft.Json.Linq;
 namespace Confiti.MoySklad.Remap.Client.Json
 {
     /// <summary>
-    /// Represents an json creation converter to object.
+    /// Represents an JSON converter to create the instance of <typeparamref name="T"/> class.
     /// </summary>
     public abstract class JsonCreationConverter<T> : JsonConverter<T>
     {
         #region Properties
 
-        /// <summary>
-        /// Gets a value indicating whether this <see cref="JsonConverter"/> can write JSON.
-        /// </summary>
-        /// <value><c>true</c> if this <see cref="JsonConverter"/> can write JSON; otherwise, <c>false</c>.</value>
+        /// <inheritdoc/>
         public override bool CanWrite => false;
 
         #endregion Properties
 
         #region Methods
 
-        /// <summary>
-        /// Reads the JSON representation of the object.
-        /// </summary>
-        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
-        /// <param name="objectType">Type of the object.</param>
-        /// <param name="existingValue">The existing value of object being read. If there is no existing value then <c>null</c> will be used.</param>
-        /// <param name="hasExistingValue">The existing value has a value.</param>
-        /// <param name="serializer">The calling serializer.</param>
-        /// <returns>The object value.</returns>
+        /// <inheritdoc/>
         public override T ReadJson(JsonReader reader, Type objectType, T existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
-            JObject jObject = JObject.Load(reader);
+            var jObject = JObject.Load(reader);
 
             T target = Create(objectType, jObject, serializer);
             serializer.Populate(jObject.CreateReader(), target);
 
+            PostPopulate(target, serializer);
+
             return target;
         }
 
-        /// <summary>
-        /// Writes the JSON representation of the object.
-        /// </summary>
-        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
-        /// <param name="value">The value.</param>
-        /// <param name="serializer">The calling serializer.</param>
+        /// <inheritdoc/>
         public override sealed void WriteJson(JsonWriter writer, T value, JsonSerializer serializer)
         {
             throw new NotSupportedException($"'{nameof(JsonCreationConverter<T>)}' should only be used while deserializing.");
@@ -59,6 +45,15 @@ namespace Confiti.MoySklad.Remap.Client.Json
         /// <param name="serializer">The calling serializer.</param>
         /// <returns>The object value.</returns>
         protected abstract T Create(Type objectType, JObject jObject, JsonSerializer serializer);
+
+        /// <summary>
+        /// Called after the target object has been populated with JSON values.
+        /// </summary>
+        /// <param name="value">The populated object.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        protected virtual void PostPopulate(T value, JsonSerializer serializer)
+        {
+        }
 
         #endregion Methods
     }
