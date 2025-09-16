a3fc95c620577efe5932ddbacdb2e1e7a77b21de
dkindev <omigoneo@gmail.com>
2025-09-16 16:16:02 +0300

    Remove unnecessary test from OAuthApiTests

--- src/Remap.Sdk/test/Remap.Sdk.IntegrationTests/Api/OAuthApiTests.cs
+++ src/Remap.Sdk/test/Remap.Sdk.IntegrationTests/Api/OAuthApiTests.cs
@@ -12,7 +12,7 @@ namespace Confiti.MoySklad.Remap.IntegrationTests.Api
         #region Methods
 
         [Test]
-        public async Task ObtainTokenAsync_should_return_access_token()
+        public async Task GetAsync_should_return_access_token()
         {
             var response = await _subject.GetAsync();
 
@@ -20,7 +20,7 @@ namespace Confiti.MoySklad.Remap.IntegrationTests.Api
         }
 
         [Test]
-        public async Task ObtainTokenAsync_should_return_status_code_200_or_201()
+        public async Task GetAsync_should_return_status_code_200_or_201()
         {
             var response = await _subject.GetAsync();
 
@@ -28,27 +28,13 @@ namespace Confiti.MoySklad.Remap.IntegrationTests.Api
         }
 
         [Test]
-        public async Task ObtainTokenAsync_with_invalid_password_should_return_status_code_401()
-        {
-            _credentials.Password = null;
-
-            try
-            {
-                await _subject.GetAsync();
-            }
-            catch (ApiException e)
-            {
-                e.ErrorCode.Should().Be(401);
-            }
-        }
-
-        [Test]
-        public async Task ObtainTokenAsync_with_invalid_password_should_throw_api_exception()
+        public async Task GetAsync_with_invalid_password_should_throw_api_exception()
         {
             _credentials.Password = null;
 
             Func<Task> getAccessToken = () => _subject.GetAsync();
-            await getAccessToken.Should().ThrowAsync<ApiException>();
+            var apiException = await getAccessToken.Should().ThrowAsync<ApiException>();
+            apiException.And.ErrorCode.Should().Be(401);
         }
 
         #endregion Methods
